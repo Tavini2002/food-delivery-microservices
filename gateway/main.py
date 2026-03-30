aff9aa4e6d661444e77c569ac8286e56c6ca1a84
dinusha <164065372+dilakshitha@users.noreply.github.com>
2026-03-30 12:54:46 +0530

    Gateway routes

--- gateway/main.py
+++ gateway/main.py
@@ -39,4 +39,26 @@ async def create_restaurant(req: Request):
     body = await req.json()
     return await forward_request("restaurant", "/api/restaurants", "POST", body)
 
+@app.get("/gateway/menu")
+async def get_all():
+    return await forward_request("menu", "/api/menu", "GET")
+
+@app.post("/gateway/menu")
+async def create(req: Request):
+    body = await req.json()
+    return await forward_request("menu", "/api/menu", "POST", body)
+
+@app.get("/gateway/menu/{id}")
+async def get_one(id: int):
+    return await forward_request("menu", f"/api/menu/{id}", "GET")
+
+@app.put("/gateway/menu/{id}")
+async def update(id: int, req: Request):
+    body = await req.json()
+    return await forward_request("menu", f"/api/menu/{id}", "PUT", body)
+
+@app.delete("/gateway/menu/{id}")
+async def delete(id: int):
+    return await forward_request("menu", f"/api/menu/{id}", "DELETE")
+
 #--------------------------------------------------------------------------------------
